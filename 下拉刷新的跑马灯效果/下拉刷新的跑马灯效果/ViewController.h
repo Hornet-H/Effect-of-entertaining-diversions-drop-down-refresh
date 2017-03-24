a64a821b7491ad34e6f2545cf12dcd3e3dff6a27
黄来峰 <751362521@qq.com>
2017-03-24 12:27:00 +0800

    创建工程

---
+++ 下拉刷新的跑马灯效果/下拉刷新的跑马灯效果/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  下拉刷新的跑马灯效果
+//
+//  Created by 黄来峰 on 2017/3/24.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ 下拉刷新的跑马灯效果/下拉刷新的跑马灯效果/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  下拉刷新的跑马灯效果
+//
+//  Created by 黄来峰 on 2017/3/24.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
