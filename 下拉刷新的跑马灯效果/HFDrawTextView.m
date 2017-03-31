278cbba1f4bf7e29741e5f2561238fd5e1c65399
黄来峰 <751362521@qq.com>
2017-03-31 22:13:39 +0800

    实现下来刷新的跑马灯效果

---
+++ 下拉刷新的跑马灯效果/HFDrawTextView.m
@@ -0,0 +1,21 @@
+//
+//  HFDrawTextView.m
+//  下拉刷新的跑马灯效果
+//
+//  Created by 黄来峰 on 2017/3/31.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#import "HFDrawTextView.h"
+
+@implementation HFDrawTextView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end
---
+++ 下拉刷新的跑马灯效果/下拉刷新的跑马灯效果/UIScrollView+HFRefresh.h
@@ -0,0 +1,13 @@
+//
+//  UIScrollView+HFRefresh.h
+//  下拉刷新的跑马灯效果
+//
+//  Created by 黄来峰 on 2017/3/31.
+//  Copyright © 2017年 com.gmw.love. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIScrollView (HFRefresh)
+
+@end
